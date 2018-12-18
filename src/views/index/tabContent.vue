207dd490cc88fff8ea79390416ef7b83359a09ba
Hillvy <1563267894@qq.com>
2018-12-19 00:57:20 +0800

    ...

--- src/views/index/tabContent.vue
+++ src/views/index/tabContent.vue
@@ -2,11 +2,11 @@
 <template>
   <div id="tabContent" class="wrapper page-index">
     <div class="white mgt10 pd10">
-        <scroller lock-x :scrollbar-y=false height="-60" use-pullup @on-scroll-bottom="loadMore" @on-pulldown-loading="refresh" v-model="status" ref="scroller">
+        <scroller lock-x :scrollbar-y=false height="-60" use-pullup use-pulldown @on-scroll-bottom="loadMore" @on-pulldown-loading="refresh" v-model="status" ref="scroller">
           <div>
             <div class="white">
              <grid :cols="4" :show-lr-borders="false">
-              <grid-item  v-model="index3" class="vux-center" :label="item" v-for="(item,index3) in list3"  @on-item-click="onTabsClick(item)" :key="index3">
+              <grid-item  v-model="index3" class="vux-center" :label="item.className" v-for="(item,index3) in list3"  @on-item-click="onTabsClick(item)" :key="index3">
                 <img slot="icon" :src="swiperPic">
               </grid-item>
             </grid> 
@@ -21,13 +21,13 @@
             </flexbox-item> 
           </flexbox>
         </div>
+        </div>
         
         <div slot="pullup" class="xs-plugin-pullup-container xs-plugin-pullup-up" style="position: absolute; width: 100%; height: 40px; bottom: -40px; text-align: center;">
           <span v-show="status.pullupStatus === 'default'"></span>
-          <span class="pullup-arrow" v-show="status.pullupStatus === 'down' || status.pullupStatus === 'up'" :class="{'rotate': status.pullupStatus === 'up'}">↑</span>
+          <span class="pullup-arrow" v-show="status.pullupStatus === 'up'" :class="{'rotate': status.pullupStatus === 'up'}">↑</span>
           <span v-show="status.pullupStatus === 'loading'"><spinner type="ios-small"></spinner></span>
           <span v-show="status.pullupStatus === 'complete'">已加载完成</span>
-        </div>
         </div>
       </scroller>
     </div>
@@ -36,11 +36,11 @@
 
 <script>
 import { Tab, TabItem, Swiper, SwiperItem, Grid, GridItem, Scroller, Spinner, Flexbox, FlexboxItem } from 'vux';
-const list3 = () => ['新鲜水果', '精选肉类', '海鲜水产', '新鲜蔬菜', '蛋类产品']
 import vTitle from "@/components/v-title";
 import swiperPic from "@/assets/images/banner@2x.png";
 const qs = require("qs");
 export default {
+  props : ['parentId'],// 接收父组件传过来的值
   data() {
     return {
       urlPic:this.api.urlPic,
@@ -49,12 +49,11 @@ export default {
       listData:[],
       pullupEnabled: true,
       status: {
-        pullupStatus: 'default',
-        pulldownStatus: 'default'
+        pullupStatus: 'default'
       },
       swiperPic,
-      list3: list3(),
-      items:'新鲜水果',
+      list3: [],
+      items:'',
       index3: 0,
       bannerSwiper: {
         speed: 800,
@@ -95,37 +94,29 @@ export default {
     // 读取用户其他数据
     this.getBannerData();
     this.loadMore();
+    this.getTab(this.parentId);
+    console.log(this.parentId);
   },
   methods: {
+    getTab (id){
+      this.$axios.get(this.api.getChildsList+id)
+       .then(res => {
+        if(res.data.code === 1){
+           console.log(res.data);
+           if(res.data.content.length>0){
+             this.list3 = res.data.content;
+             this.items = res.data.content[0].className;
+           }
+        }
+      })
+      .catch(res => {
+       //下单失败，请您稍后重试
+      });
+    },
     loadMore (index) {
-      if(index==1){
-        this.currentPage = 0;this.totalPage = 1;this.listData=[];
-        if(this.currentPage< this.totalPage){
-            this.currentPage= parseInt(this.currentPage) + 1;
-            this.$axios.post(this.api.getGoodsList,qs.parse({ "page" : this.currentPage, "limit":8 }),{headers: {"content-type": "application/json"}})
-            .then(res => {
-               console.log(res.data);
-               this.totalPage=res.data.content.totalPage; 
-               this.listData = [...this.listData,...res.data.content.list];
-               console.log(this.listData);
-            })
-            .catch(res => {
-             //下单失败，请您稍后重试
-            });
-        }else{
-            this.status.pullupStatus = 'complete';
-            setTimeout(()=>{
-              this.status.pullupStatus = 'default';
-            },1000);
-            console.log('已加载完');
-          }
-          setTimeout(() => {
-            this.$refs.scroller.donePullup()
-          }, 10) 
-      }else{
         if(this.currentPage< this.totalPage){
           this.currentPage= parseInt(this.currentPage) + 1;
-          this.$axios.post(this.api.getGoodsList,qs.parse({ "page" : this.currentPage, "limit":8 }),{headers: {"content-type": "application/json"}})
+          this.$axios.post(this.api.getGoodsList,qs.parse({'goodsName':'上', "page" : this.currentPage, "limit":8 }),{headers: {"content-type": "application/json"}})
           .then(res => {
              console.log(res.data);
              this.totalPage=res.data.content.totalPage; 
@@ -143,18 +134,8 @@ export default {
           console.log('已加载完');
         }
         setTimeout(() => {
-          this.$refs.scroller.donePullup()
+        //  this.$refs.scroller.donePullup()
         }, 10)
-      }
-    },
-    refresh () {
-       console.log('刷新');
-       this.$nextTick(() => {
-          setTimeout(() => {
-            this.$refs.scroller.donePulldown()
-            this.pullupEnabled && this.$refs.scroller.enablePullup()
-          }, 10)
-        })
     },
     onTabsClick(item){
       this.items = item;
@@ -169,7 +150,16 @@ export default {
         
         }
       });
-    }
+    },
+    refresh () {
+       console.log('刷新');
+       this.$nextTick(() => {
+          setTimeout(() => {
+            this.$refs.scroller.donePulldown()
+            this.pullupEnabled && this.$refs.scroller.enablePullup()
+          }, 10)
+        })
+    } 
   }
 };
 </script>
